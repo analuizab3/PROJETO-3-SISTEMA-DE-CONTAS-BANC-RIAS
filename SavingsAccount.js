b411e29c6cf005b38f5b881b40df14d2c2a046bb
Ana Luiza <160125249+analuizab3@users.noreply.github.com>
2024-09-23 10:43:56 -0300

    Update SavingsAccount.js

--- SavingsAccount.js
+++ SavingsAccount.js
@@ -1,17 +1,39 @@
-const BankAccount = require('./BankAccount')
-
-class SavingsAccount extends BankAccount {
-    constructor(accountHolder, interestRate) {
-        super(accountHolder);
-        this.interestRate = interestRate;
+class SavingsAccount {
+    constructor(accountHolder, interestRate, initialBalance = 0) {
+      this.accountHolder = accountHolder
+      this.interestRate = interestRate
+      this.balance = initialBalance
     }
-
+ 
+    getBalance() {
+      return this.balance
+    }
+ 
+    deposit(amount) {
+      this.balance += amount
+    }
+ 
+    withdraw(amount) {
+      if (this.balance >= amount) {
+        this.balance -= amount
+        console.log(`${this.accountHolder} sacou R$${amount.toFixed(2)} | Saldo total: R$${this.balance.toFixed(2)}`)
+        return true
+      } else {
+        console.log(`Saldo insuficiente para ${this.accountHolder}`);
+        return false
+      }
+    }
+ 
+    getAccountHolder() {
+      return this.accountHolder
+    }
+ 
     applyInterest() {
-        const interest = this.balance * (this.interestRate / 100);
-        this.balance += interest;
-        console.log(`Saldo total de ${this.accountHolder}: R$${this.balance.toFixed(2)}.00 | Aplicado ${this.interestRate}% de juros`);
-        console.log(`Novo saldo total: R$${this.balance.toFixed(2)}.00`);
+      const interest = this.getBalance() * (this.interestRate / 100)
+      this.deposit(interest)
+      console.log(`Saldo total de ${this.getAccountHolder()}: R$${(this.getBalance() - interest).toFixed(2)} | Aplicado ${this.interestRate}% de juros`)
+      console.log(`Novo saldo total: R$${this.getBalance().toFixed(2)}`)
     }
-}
-
-module.exports = SavingsAccount
+  }
+ 
+  module.exports = SavingsAccount
